fc68b5c7f0d8b7ffce639ab8f4e549f19e7291b7
MarlonTo <mptorres4@espe.edu.ec>
2023-02-25 08:57:20 -0500

    modificaciones
    modificaciones

--- ProyectoFinal/JS/script1.js
+++ ProyectoFinal/JS/script1.js
@@ -6,44 +6,39 @@ form.addEventListener("submit", e=>{
     e.preventDefault();
 	let regexNombre = /^[A-Za-z\s]{3,25}$/
     
+    
     var entrar = false;
-    parrafo.innerHTML = ""
+    parrafo.innerHTML = "";
 	var datos= new Array();
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(){
-		if (this.readyState ==4  && this.status==200){
-			var warnings = ""
-			datos=this.responseText.split(",");	
-			var user = document.getElementById("user").value;
-			var password = document.getElementById("password").value;
-			for(var i=4; i< datos.length;i=i+7){
-				if((user!= "admin") || (password != "admin") ){
-					warnings = `Usuario o contraseña no es correcto.<br>`
-					entrar = true;
-				}else{
-					i=datos.length;
-					entrar = false;
-					break;
-				}
-				console.log(datos[i+2])
-				if(datos[i+2] == "activo"){
-					if(((user != datos[i]) || (password != datos[i+1]))){
-						warnings = `Usuario o contraseña no es correcto.<br>`
-						entrar = true;
-					}else{
-						i=datos.length;
-						entrar = false;
-						break;
-					};
-				}		
-			}
-			if(entrar){
-				parrafo.innerHTML = warnings;
-			}else{
-				window.location.href = "HTML/inicio.html";
-				
-				form.reset();
-			}
-		} 
-	}
+	
+	 validaClave();
+
+  
 });      
+function validaClave(){
+	
+		var warnings = ""
+		
+		var user = document.getElementById("user").value;
+		var password = document.getElementById("password").value;
+		
+		
+		if((user == "admin") && (password == "admin") ){
+			entrar = true;
+		}else{
+			warnings = `Usuario o contraseña no es correcto.<br>`
+			entrar = false;
+			
+		}
+			
+				
+	
+		if(entrar){
+			window.location.href = "HTML/inicio.html";
+			form.reset();
+		}else{
+			parrafo.innerHTML = warnings;
+			
+		}
+
+}
